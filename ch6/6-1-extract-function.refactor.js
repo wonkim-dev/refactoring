89de91c617ef799f3a66df579e3452bf7178b79a
wonkim-dev <wonkim.dev@gmail.com>
2022-07-03 10:14:36 +0200

    refactor: extract function

---
+++ ch6/6-1-extract-function.refactor.js
@@ -0,0 +1,33 @@
+export function printOwing(invoice) {
+	printBanner();
+	let outstanding = calculateOutstanding(invoice);
+	recordDueDate(invoice);
+	printDetails(invoice, outstanding);
+}
+
+function printBanner() {
+	console.log('***********************');
+	console.log('**** Customer Owes ****');
+	console.log('***********************');
+}
+
+function calculateOutstanding(invoice) {
+	return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
+
+function recordDueDate(invoice) {
+	const today = new Date();
+	invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
+}
+
+function printDetails(invoice, outstanding) {
+	console.log(`name: ${invoice.customer}`);
+	console.log(`amount: ${outstanding}`);
+	console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+const invoice = {
+	orders: [{ amount: 2 }, { amount: 5 }],
+	customer: '엘리',
+};
+printOwing(invoice);
